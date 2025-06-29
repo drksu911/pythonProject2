50604c3f5205b2fc432210a80629a1cb1a012555
Kseniya Druzhinina <drksu91@mail.ru>
2025-06-30 00:19:57 +0300

    Finaly_version_9.2

--- src/masks.py
+++ src/masks.py
@@ -5,8 +5,8 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
 
     card_str = str(card_number).strip()
-    name_pay = ''
-    number_pay =''
+    name_pay = ""
+    number_pay = ""
     for symbol in card_str:
         if symbol.isdigit():
             number_pay += symbol
@@ -22,10 +22,3 @@ def get_mask_account(account_number: Union[str, int]) -> str:
     account_str = str(account_number).strip()
     masked = f"Счет **{account_str[-4:]}"
     return masked
-
-
-card_number = "Visa Platinum 7000792289606361"
-account_number = "Счет 73654108430135874305"
-
-#print(get_mask_card_number(card_number))
-#print(get_mask_account(account_number))
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
+
 
 def mask_account_card(number_acc_or_card: str) -> str:
     """Функция обработки информации о картах и счетах"""
@@ -11,15 +12,15 @@ def mask_account_card(number_acc_or_card: str) -> str:
 
 
 def get_date(date_info: str) -> str:
-    """Функция форматирования даты"""
+    """Функция форматирования даты ДД.ММ.ГГГГ"""
 
     date_str = date_info.strip()
     masked = f"{date_str[8:10]}.{date_str[5:7]}.{date_str[:4]}"
     return masked
 
 
-
-number_acc_or_card = input('Введите номер счета или номер карты')
+print("Введите номер счета или номер карты", sep="\n")
+number_acc_or_card = input()
 date_info = "2024-03-11T02:26:18.671407"
-print(mask_account_card(number_acc_or_card ))
+print(mask_account_card(number_acc_or_card))
 print(get_date(date_info))
